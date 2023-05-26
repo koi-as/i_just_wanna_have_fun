4bebd7375d2c37700d15ef714890131bcbbd38e1
Koi-AS <lightningblade.901@gmail.com>
2023-05-26 02:58:53 -0400

    installed express-handlebars & added some helpful comments

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
+// npm packages
 const express = require('express');
+const exphbs = require('express-handlebars');
+// other files
 
+// create port and app
 const app = express();
 const PORT = 9352 || process.env.PORT;
+// set up handlebars
+const hbs = exphbs.create({});
 
-app.use(express.urlencoded({ extended: true }));
+// create post requests
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+// handlebars is the default template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+// start the server
 app.listen(PORT, () => console.log(`Server running at localhost:${PORT}`));
